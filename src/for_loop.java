d23001377c3296b37977f1bd71bd442fbe0a3b16
Rashmitha-nr <gowdarashmithagowda20032003@gmail.com>
2025-06-30 12:33:14 +0530

    last commit

---
+++ src/for_loop.java
@@ -0,0 +1,14 @@
+public class for_loop {
+    public static void main(String[] args) {
+        
+        for(int i = 0; i<4; i++){
+            System.out.println(i);
+        }
+        System.out.println();
+        for(int i=5;i<10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
+
